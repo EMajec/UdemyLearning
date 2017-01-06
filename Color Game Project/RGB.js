6436655d3007f4885e132e260957ba7108e3ed41
Ethan Turner <EMajec@users.noreply.github.com>
2017-01-06 14:53:47 -0600

    Final DOM Practice
    This is mainly JS and was my first major project with Javascript as the core. Little html, little CSS, and lots of manipulating items. I learned transition syntax, was able to refactor my code from 200 lines down to 100.

---
+++ Color Game Project/RGB.js
@@ -0,0 +1,102 @@
+//All Variables
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var message = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+var colorDisplay = document.getElementById("colordisplay");
+
+// All Functions
+
+function init() {
+	setUpModeButtons()
+	setUpSquares()
+	reset();
+}
+
+function setUpSquares() {
+		for (var i = 0; i < squares.length; i++) {
+	squares[i].addEventListener("click", function(){
+		var clickedColor = this.style.background;
+		if (clickedColor === pickedColor) {
+			message.textContent ="Correct!";
+			h1.style.background = clickedColor
+			changeColors(clickedColor)
+			resetButton.textContent = "Play Again?"
+		}
+		else {
+			this.style.background = "#232323";
+			message.textContent = "Try Again"
+			}
+		});
+	}
+}
+	
+
+function setUpModeButtons() {
+	for (var i = 0; i < modeButtons.length; i++) {
+		modeButtons[i].addEventListener("click", function(){
+			modeButtons[0].classList.remove("selected")
+			modeButtons[1].classList.remove("selected")
+			this.classList.add("selected");
+			this.textContent === "Easy" ? numSquares = 3: numSquares = 6;
+			reset();
+	});
+	}
+}
+
+function changeColors(color){
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.background = color;
+	}
+
+}
+
+function pickColor() {
+	var random = Math.floor(Math.random() *colors.length)
+	return colors[random];
+}
+
+function generateRandomColor(num) {
+	var arr = []
+	for (var i = 0; i < num; i++) {
+		arr.push(randomCOlor())
+	}	
+	return arr;
+}
+
+function randomCOlor() {
+	var r = Math.floor(Math.random() *  256)
+	var g = Math.floor(Math.random() *  256)
+	var b = Math.floor(Math.random() *  256)
+
+	return "rgb(" + r + ", " + g + ", " + b +")";
+}
+
+function reset() {
+	colors = generateRandomColor(numSquares);
+	pickedColor = pickColor();
+	colordisplay.textContent = pickedColor;
+	resetButton.textContent = "New Colors"
+	message.textContent = "";
+	for (var i = 0; i < squares.length; i++) {
+		if (colors[i]) {
+			squares[i].style.display = "block";
+			squares[i].style.background = colors[i];
+		}
+		else {
+			squares[i].style.display = "none";
+		}
+	}
+	h1.style.background = "steelblue"
+}
+
+resetButton.addEventListener("click", function(){
+	reset();
+})
+
+// Run the Game
+init();
